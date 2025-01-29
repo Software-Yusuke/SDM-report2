31db4a8db0557bfcd6c0163059b992dfdd070f36
YuEnu-creator <nakamura.yusuke.bc@tut.ac.jp>
2025-01-29 16:53:15 +0900

    git addし忘れ
    test_casesの新しく作成し忘れてた.

---
+++ .history/calc_mul_20250129155702.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import re
+                
+def calc(A,B):
+        ai=str(A)
+        bi=str(B)
+        p = re.compile('\d+(\.\d+)?')
+        if p.match(ai) and p.match(bi):
+                a=int(ai)
+                b=int(bi)
+                if 0<a<1000 and 0<b<1000:
+                        valid=True
+                else:
+                        valid=False
+        else:
+                valid=False
+                
+        if valid:
+                ans=a*b
+                return ans
+        else:
+                return -1
+        
+                
+def main ():
+	matchstring = ''
+	while matchstring != 'end':
+                A = input ('input A: ')
+                B = input ('input B: ')
+                print ('input A * input B = ', calc(A,B))
+
+if __name__ == '__main__':
+	main()
