92f5a5fe4248e0b6cb6156a0ad675bbdba7e6349
YuEnu-creator <nakamura.yusuke.bc@tut.ac.jp>
2025-02-05 15:36:51 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -19,7 +19,7 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
         
-        #境界値とのテスト 追加
+        #有効同値とのテスト 追加
         def test_sample5 (self):
                 self.assertEqual (1998, calc(2, 999))
         
@@ -28,30 +28,38 @@ class TestCalc (unittest.TestCase):
         
         def test_sample7 (self):
                 self.assertEqual (999, calc(1,999))
-        
+
+        #境界同値とのテスト 追加   
         def test_sample8 (self):
-                self.assertEqual (-1, calc(100,1000))
+                self.assertEqual (1, calc(1,1))
 
         def test_sample9 (self):
-                self.assertEqual (-1, calc(0, 1000))
-                
-        #無効同値のテスト 追加
-        def test_sample10 (self):
-                self.assertEqual (-1, calc('AB', 'CD'))
-        
-        def test_sample11 (self):
-                self.assertEqual (-1, calc('A13', 999))
+                self.assertEqual (998001, calc(999, 999))
 
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(0, 0))
+
+        def test_sample11 (self):
+                self.assertEqual (-1, calc(1000, 1000))
+
+        #無効同値のテスト 追加 
         def test_sample12 (self):
                 self.assertEqual (-1, calc(-1, 999))
-                
-        def test_sample13 (self):
-                self.assertEqual (-1, calc(0.1, 300))
 
+        def test_sample13 (self):
+                self.assertEqual (-1, calc(20, 3000))
+
+        #その他 (文字列,小数点, 片方の値が記入無しなど)
         def test_sample14 (self):
                 self.assertEqual (-1, calc('B', 998))
 
         def test_sample15 (self):
-                self.assertEqual (-1, calc(0, 1500))
+                self.assertEqual (-1, calc(0.123, 500))
+
+        def test_sample16(self):
+                self.assertEqual (-1, calc(1, None))
+        
+        def test_sample17(self):
+                self.assertEqual (-1, calc(20, 'a1'))
 
 
