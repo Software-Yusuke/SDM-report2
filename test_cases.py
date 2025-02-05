59a2bab3388475d5b9b80355fdf649533d3c3a96
YuEnu-creator <nakamura.yusuke.bc@tut.ac.jp>
2025-02-05 15:57:31 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -29,7 +29,7 @@ class TestCalc (unittest.TestCase):
         def test_sample7 (self):
                 self.assertEqual (999, calc(1,999))
 
-        #境界同値とのテスト 追加   
+        #境界値とのテスト 追加   
         def test_sample8 (self):
                 self.assertEqual (1, calc(1,1))
 
